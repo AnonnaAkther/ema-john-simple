519bea58d920e77040b6f9d5339716f024c7a2b1
unknown <ah53622@gmail.com>
2021-10-27 11:05:56 +0600

    plceorder commit

--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -3,16 +3,25 @@ import useProducts from '../../hooks/useProduct';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useHistory } from 'react-router';
 
 const OrderReview = () => {
     const [products] = useProducts();
     const [cart, setCart] = useCart(products);
+    const history = useHistory();
     const handleRemove = key => {
          const newCart = cart.filter(product => product.key !== key);
          setCart(newCart);
          removeFromDb(key);
     }
+
+    const handlePlaceOrder = () => {
+        history.push('/placeorder');
+        setCart([]);
+        clearTheCart();
+    }
+
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -26,7 +35,7 @@ const OrderReview = () => {
             </div>
             <div className="cart-container">
             <Cart cart={cart}>
-                <button className="btn-regular">Place Order</button>
+                <button onClick={handlePlaceOrder} className="btn-regular">Place Order</button>
             </Cart>
             </div>
         </div>
